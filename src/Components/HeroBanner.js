2c9a4be74b1b4e52cba07a84088e87f77a0f439b
Guna-developer <guna.code10@gmail.com>
2024-12-04 17:52:28 +0530

    home-shop

--- src/Components/HeroBanner.js
+++ src/Components/HeroBanner.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './HeroBanner.css';
+import { useNavigate } from 'react-router-dom';
 
 function HeroBanner() {
+  const navigate = useNavigate();
+  const handleShop = ()=>{
+navigate("/shopnow")
+  }
   return (
     <section className="hero-banner">
       <div className="banner-content">
         <h2>Discover the Best Deals on Mobile Phones</h2>
         <p>Your one-stop shop for the latest smartphones and accessories.</p>
-        <a href="/shopnow" className="cta-button">Shop Now</a>
+        <div onClick={handleShop} href="/shopnow" className="cta-button">Shop Now</div>
       </div>
     </section>
   );
