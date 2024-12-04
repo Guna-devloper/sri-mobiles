b05cfd36c7ac6740312accd21fa8780ef3e65b3a
Guna-developer <guna.code10@gmail.com>
2024-12-04 15:55:40 +0530

    routing

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <header className="header">
       <h1 className="logo">Sri Mobiles</h1>
       <nav className="navbar">
-        <a href="/">Home</a>
-        <a href="/shopnow">Products</a>
+        <Link to="/" >Home</Link>
+        <Link to="/shopnow">Products</Link>
         <a href="/about">About Us</a>
         <a href="/contact">Contact</a>
       </nav>
